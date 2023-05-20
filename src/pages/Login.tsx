8732688f9c51efe4bc8b53be217931c0f6767004
searxh <search35453@gmail.com>
2023-05-20 20:41:04 +0700

    Separated Login and Register pages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-export default function LoginRegister() {
+const Login: React.FC = () => {
   return (
     <>
       <nav className="navbar navbar-light">
@@ -43,26 +43,19 @@ export default function LoginRegister() {
         <div className="container page">
           <div className="row">
             <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign up</h1>
+              <h1 className="text-xs-center">Sign in</h1>
               <p className="text-xs-center">
-                <a href="">Have an account?</a>
+                <a href="/#/register">Need an account?</a>
               </p>
 
-              <ul className="error-messages">
-                <li>That email is already taken</li>
-              </ul>
-
               <form>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
-                </fieldset>
                 <fieldset className="form-group">
                   <input className="form-control form-control-lg" type="text" placeholder="Email" />
                 </fieldset>
                 <fieldset className="form-group">
                   <input className="form-control form-control-lg" type="password" placeholder="Password" />
                 </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+                <button className="btn btn-lg btn-primary pull-xs-right">Sign in</button>
               </form>
             </div>
           </div>
@@ -82,4 +75,6 @@ export default function LoginRegister() {
       </footer>
     </>
   );
-}
+};
+
+export default Login;
